a3b20c9c0f44a78d94d3658498b858f7adf198b1
David Mears <clarinet.david@gmail.com>
2024-06-27 12:38:31 +0100

    Try some Nuxt config, notably changing the nitro Node mode
    This should allow multi-core handling.
    https://nitro.unjs.io/deploy/runtimes/node
    
    It is not considered compatible with websockets.
    https://github.com/unjs/nitro/issues/2171

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  debug: true,
+  features: { devLogs: true },
+  nitro: { preset: 'node-cluster' },
 })
