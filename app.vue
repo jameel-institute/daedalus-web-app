d60b16d06f55694ca6ca2c55342ec99c87600efc
David Mears <clarinet.david@gmail.com>
2024-07-08 16:29:41 +0100

    More config, and install Pinia

--- app.vue
+++ app.vue
@@ -8,8 +8,13 @@
 </template>
 
 <script setup>
+import '~/assets/scss/fonts.scss';
 import { iconsSet } from '@/assets/icons';
 
 const nuxtApp = useNuxtApp()
 nuxtApp.vueApp.provide('icons', iconsSet);
 </script>
+
+<style lang="scss">
+@use "~/assets/scss/main.scss";
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
     },
   },
 
-  debug: true,
+  debug: false,
   features: { devLogs: true },
 
   nitro: {
@@ -19,5 +19,17 @@ export default defineNuxtConfig({
 
   modules: [
     "@nuxtjs/fontaine", // https://nuxt.com/docs/getting-started/styling#font-advanced-optimization
-  ]
+    "@pinia/nuxt",
+  ],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/scss/_variables.scss" as *;' // Enables variables to be accessible in SFC styles. https://nuxt.com/docs/getting-started/styling#using-preprocessors
+        }
+      }
+    }
+  }
+  
 })
