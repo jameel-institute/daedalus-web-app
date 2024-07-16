e4b3c8f319c9f62630c5a84b21a98b78f24795d9
David Mears <clarinet.david@gmail.com>
2024-07-16 20:42:17 +0100

    Improve styling of breadcrumbs

--- components/BreadCrumb.vue
+++ components/BreadCrumb.vue
@@ -9,17 +9,26 @@ I expect that to change based on how we end up making routes work. -->
       :href="item.href"
       :class="index === items.length - 1 ? 'active' : ''"
     >
-      {{ item.text }}
+      <small>{{ item.text }}</small>
     </CBreadcrumbItem>
   </CBreadcrumb>
 </template>
 
 <script lang="ts" setup>
 import { useRoute } from 'vue-router'
+
 const route = useRoute()
 
 const items = route.meta.breadcrumbs || []
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+@use "sass:map";
+
+.breadcrumb {
+  .breadcrumb-item a {
+    color: rgba($imperial-blue, 0.8);
+    text-decoration: none !important;
+  }
+}
 </style>
