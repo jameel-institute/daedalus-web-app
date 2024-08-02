f078f8e4cd33391bd859d2f20ace4759994b36d2
David Mears <clarinet.david@gmail.com>
2024-08-02 13:11:40 +0100

    Use correct env var keys for 'nuxt preview' context: NUXT to NITRO

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,7 +75,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   // Multiple web servers (or background processes) can be launched: https://playwright.dev/docs/api/class-testconfig#test-config-web-server
   webServer: {
-    command: 'NUXT_HOST="127.0.0.1" NUXT_PORT="3000" npm run build && npm run start',
+    command: 'NITRO_HOST="127.0.0.1" NITRO_PORT="3000" npm run build && npm run preview',
     port: 3000,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
