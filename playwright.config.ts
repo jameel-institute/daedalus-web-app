143063c5b4399dfb8df22556febe1654f15e7d93
David Mears <clarinet.david@gmail.com>
2024-09-10 18:59:52 +0100

    Update e2e test to match latest R API build

--- playwright.config.ts
+++ playwright.config.ts
@@ -78,7 +78,7 @@ export default defineConfig({
   webServer: {
     command: "npm run build && cd .output && node ./server/index.mjs",
     url: "http://127.0.0.1:3000",
-    timeout: 120 * 1000,
+    timeout: 30 * 1000,
     reuseExistingServer: true,
     stdout: "pipe", // Debugging
   },
--- tests/e2e/runAnalysis.spec.ts
+++ tests/e2e/runAnalysis.spec.ts
@@ -10,12 +10,12 @@ test("Can request a scenario analysis run", async ({ page, baseURL }) => {
   await page.waitForURL(`${baseURL}/scenarios/new`);
 
   await expect(page.getByText("Simulate a new scenario")).toBeVisible();
-  await expect(page.getByRole("form")).toBeVisible({ timeout: 30000 });
+  await expect(page.getByRole("form")).toBeVisible();
 
   await page.selectOption('select[id="pathogen"]', { label: "Influenza 1957" });
   await page.selectOption('select[id="response"]', { label: "Elimination" });
   await page.selectOption('select[id="country"]', { label: "United States" });
-  await page.click('div[aria-label="Advance vaccine investment"] label[for="low"]');
+  await page.click('div[aria-label="Global vaccine investment"] label[for="low"]');
 
   await expect(page.getByRole("form")).toHaveAttribute("data-test-form-data", "{\"country\":\"United States\",\"pathogen\":\"influenza-1957\",\"response\":\"elimination\",\"vaccine\":\"low\"}");
 
