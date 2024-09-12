e9538589129b31f0eeee1afda74097a585ed8fd2
Emma <EmmaRussellKilburn@gmail.com>
2024-09-12 23:00:59 +0100

    update expected vaccine label

--- tests/e2e/runAnalysis.spec.ts
+++ tests/e2e/runAnalysis.spec.ts
@@ -12,7 +12,7 @@ test("Can request a scenario analysis run", async ({ page, baseURL }) => {
   await page.selectOption('select[id="pathogen"]', { label: "Influenza 1957" });
   await page.selectOption('select[id="response"]', { label: "Elimination" });
   await page.selectOption('select[id="country"]', { label: "United States" });
-  await page.click('div[aria-label="Advance vaccine investment"] label[for="medium"]');
+  await page.click('div[aria-label="Global vaccine investment"] label[for="medium"]');
 
   await page.click('button:has-text("Run")');
 
