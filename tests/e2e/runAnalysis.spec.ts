2f328df2a0f3e944f9c9c4ae803ac65f23fc722d
David Mears <clarinet.david@gmail.com>
2024-09-24 14:05:41 +0100

    Update e2e tests

--- tests/e2e/runAnalysis.spec.ts
+++ tests/e2e/runAnalysis.spec.ts
@@ -1,6 +1,14 @@
 import { expect, test } from "@playwright/test";
 import checkRApiServer from "./helpers/checkRApiServer";
 
+const parameterLabels = {
+  country: "Country",
+  pathogen: "Disease",
+  response: "Response",
+  vaccine: "Global vaccine investment",
+  hospital_capacity: "Hospital surge capacity",
+};
+
 test.beforeAll(async () => {
   checkRApiServer();
 });
@@ -14,15 +22,15 @@ test("Can request a scenario analysis run", async ({ page, baseURL, isMobile }) 
   // Reduce flakeyness of tests by waiting for evidence that the page has mounted.
   await expect(page.getByTitle(/Web app version: 0.0.1/)).toHaveCount(1);
 
-  await page.selectOption('select[aria-label="Disease"]', { label: "Influenza 1957" });
-  await page.selectOption('select[aria-label="Response"]', { label: "Elimination" });
+  await page.selectOption(`select[aria-label="${parameterLabels.pathogen}"]`, { label: "Influenza 1957" });
+  await page.selectOption(`select[aria-label="${parameterLabels.response}"]`, { label: "Elimination" });
 
-  const initialCountryValue = await page.inputValue('input[aria-label="Hospital capacity"][type="number"]');
-  await page.selectOption('select[aria-label="Country"]', { label: "United States" });
-  await expect(page.locator('input[aria-label="Hospital capacity"][type="number"]')).not.toHaveValue(initialCountryValue);
+  const initialCountryValue = await page.inputValue(`input[aria-label="${parameterLabels.hospital_capacity}"][type="number"]`);
+  await page.selectOption(`select[aria-label="${parameterLabels.country}"]`, { label: "United States" });
+  await expect(page.locator(`input[aria-label="${parameterLabels.hospital_capacity}"][type="number"]`)).not.toHaveValue(initialCountryValue);
 
-  await page.click('div[aria-label="Global vaccine investment"] label[for="medium"]');
-  await page.fill('input[aria-label="Hospital capacity"][type="number"]', "200000");
+  await page.click(`div[aria-label="${parameterLabels.vaccine}"] label[for="medium"]`);
+  await page.fill(`input[aria-label="${parameterLabels.hospital_capacity}"][type="number"]`, "200000");
 
   await page.click('button:has-text("Run")');
 
