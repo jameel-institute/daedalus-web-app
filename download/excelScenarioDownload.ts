aef80dce9920b858c2eada9920d99884c836fcc6
Emma <EmmaRussellKilburn@gmail.com>
2024-10-01 16:23:44 +0100

    add costs and interventions

--- download/excelScenarioDownload.ts
+++ download/excelScenarioDownload.ts
@@ -1,5 +1,11 @@
 import * as XLSX from "xlsx";
 import type { Scenario } from "~/types/storeTypes";
+import type { ScenarioCost } from "~/types/resultTypes";
+
+interface FlatCost {
+  id: string
+  value: number
+}
 
 export class ExcelScenarioDownload {
   private readonly _scenario: Scenario;
@@ -11,27 +17,55 @@ export class ExcelScenarioDownload {
     this._workbook = XLSX.utils.book_new();
   }
 
+  private _addDataAsSheet(data: any, sheetName: string) {
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    XLSX.utils.book_append_sheet(this._workbook, worksheet, sheetName);
+  }
+
   private _addParameters() {
-    const params = this._scenario.parameters;
-    if (params) {
-      const paramData = Object.keys(params).map((key: string) => ({
-        id: key,
-        value: params[key],
-      }));
-      const worksheet = XLSX.utils.json_to_sheet(paramData);
-      XLSX.utils.book_append_sheet(this._workbook, worksheet, "Parameters");
-    }
+    const params = this._scenario.parameters!;
+    const paramData = Object.keys(params).map((key: string) => ({
+      id: key,
+      value: params[key],
+    }));
+    this._addDataAsSheet(paramData, "Parameters");
+  }
+
+  private static _flattenCosts(costs: Array<ScenarioCost>, flattened: Array<FlatCost>) {
+    costs.forEach((cost: ScenarioCost) => {
+      flattened.push({ id: cost.id, value: cost.value });
+      if (cost.children) {
+        this._flattenCosts(cost.children, flattened);
+      }
+    });
+  }
+
+  private _addCosts() {
+    const costs = this._scenario.result.data!.costs;
+    const flattenedCosts: Array<FlatCost> = [];
+    ExcelScenarioDownload._flattenCosts(costs, flattenedCosts);
+    this._addDataAsSheet(flattenedCosts, "Costs");
+  }
+
+  private _addInterventions() {
+    this._addDataAsSheet(this._scenario.result.data!.interventions, "Interventions");
   }
 
   private _buildWorkbook() {
     this._addParameters();
+    this._addCosts();
+    this._addInterventions();
+    // TODO: add time_series
   }
 
   public download() {
     try {
+      if (!this._scenario.parameters && !this._scenario.result.data) {
+        throw new Error("Cannot download scenario with no data.");
+      }
       this._buildWorkbook();
-      // TODO: generate a sensible filename from param values
-      XLSX.writeFile(this._workbook, "test_daedalus.xlsx");
+      const paramValues = Object.values(this._scenario.parameters).join("_");
+      XLSX.writeFile(this._workbook, `daedalus_${paramValues}.xlsx`);
     } catch (e) {
       // TODO: Set error in store, and display
     }
