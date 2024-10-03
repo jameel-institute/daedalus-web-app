921f4fc23078f94bc876c647c8f8be333b4e22ef
David Mears <clarinet.david@gmail.com>
2024-10-03 13:15:14 +0100

    Restyle excel download button

--- components/DownloadExcel.vue
+++ components/DownloadExcel.vue
@@ -1,10 +1,10 @@
 <template>
-  <div v-show="appStore.currentScenario.parameters" class="d-inline-block ms-2 align-content-center">
+  <div v-show="appStore.currentScenario.parameters" class="d-inline-block ms-auto align-content-center">
     <CSpinner v-if="appStore.downloading" size="sm" class="ms-2" />
     <CTooltip v-else content="Download as Excel file" placement="top">
       <template #toggler="{ togglerId, on }">
         <CButton color="light" :aria-describedby="togglerId" v-on="on" @click="appStore.downloadExcel()">
-          <CIcon icon="cilCloudDownload" size="lg" />
+          <CIcon icon="cilCloudDownload" size="lg" class="text-secondary" />
         </CButton>
       </template>
     </CTooltip>
@@ -25,3 +25,16 @@ watch(() => appStore.downloading, () => {
   alertDismissed.value = false;
 });
 </script>
+
+<style lang="scss" scoped>
+.btn {
+  padding-bottom: 0;
+  height: 100%;
+  border: 1px solid rgba(8, 10, 12, 0.17); // copying from card
+  border-radius: 0.375rem; // copying from card
+
+  &:not(:hover) {
+    background: rgba(255, 255, 255, 0.5);
+  }
+}
+</style>
