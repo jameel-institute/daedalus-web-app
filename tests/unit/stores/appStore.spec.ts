7c67177e38f4ddf5b6de040f25767eb9c3e58db2
Emma <EmmaRussellKilburn@gmail.com>
2024-10-07 17:08:10 +0100

    fix unit tests

--- tests/unit/stores/appStore.spec.ts
+++ tests/unit/stores/appStore.spec.ts
@@ -1,6 +1,3 @@
-import { useAppStore } from "@/stores/appStore";
-import { emptyScenario, mockResultData } from "@/tests/unit/mocks/mockPinia";
-import { registerEndpoint } from "@nuxt/test-utils/runtime";
 import { waitFor } from "@testing-library/vue";
 import { createPinia, setActivePinia } from "pinia";
 import { beforeEach, describe, expect, it } from "vitest";
