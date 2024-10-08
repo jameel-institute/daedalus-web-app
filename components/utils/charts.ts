68ae70595738887f879bd8b896096d3cb577c417
David Mears <clarinet.david@gmail.com>
2024-10-08 18:25:13 +0100

    WIP (adapted from original branch jidea-105-show-costs)

--- components/utils/charts.ts
+++ components/utils/charts.ts
@@ -7,5 +7,5 @@ colorRgba.alpha = 0.3;
 export const plotBandsColor = `rgba(${Object.values(colorRgba).join(",")})`;
 export const plotLinesColor = "#FF0000"; // red;
 export const timeSeriesColors = originalHighchartsColors!.slice(1);
-let costsPieColorsBase = originalHighchartsColors!.slice(2); // Skip first two colors as being too vibrant
+const costsPieColorsBase = originalHighchartsColors!.slice(2); // Skip first two colors as being too vibrant
 export const costsPieColors = ["rgba(1,1,1,0.1)"].concat(costsPieColorsBase); // Make the center circle translucent
