571b92ac528c017db940f6db0642d55ce60aae8b
David Mears <clarinet.david@gmail.com>
2024-10-10 17:16:48 +0100

    Small refactors in TimeSeries, including scoping the styles

--- components/utils/charts.ts
+++ components/utils/charts.ts
@@ -6,4 +6,6 @@ const colorRgba = hexRgb(originalHighchartsColors[0]);
 colorRgba.alpha = 0.3;
 export const plotBandsColor = `rgba(${Object.values(colorRgba).join(",")})`;
 export const plotLinesColor = "#FF0000"; // red;
-export const highchartsColors = originalHighchartsColors!.slice(1);
+export const timeSeriesColors = originalHighchartsColors!.slice(1);
+const costsPieColorsBase = originalHighchartsColors!.slice(2); // Skip first two colors as being too vibrant
+export const costsPieColors = ["rgba(1,1,1,0.1)"].concat(costsPieColorsBase); // Make the center circle translucent
