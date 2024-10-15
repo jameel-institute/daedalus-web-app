82f4ce2e42ad55bd7ae95e6be8acbc96c5520707
David Mears <clarinet.david@gmail.com>
2024-10-15 15:04:04 +0100

    Rename cost container id and fix time series component test suite

--- tests/unit/components/CostsPie.spec.ts
+++ tests/unit/components/CostsPie.spec.ts
@@ -63,7 +63,7 @@ describe("costs card", () => {
       props: { hideTooltips: false, pieSize: 100 },
     });
 
-    const container = component.find(`#costsPieContainerId`);
+    const container = component.find(`#costsPieContainer`);
     expect(container.classes()).not.toContain("hide-tooltips");
 
     component.setProps({ hideTooltips: true });
@@ -107,7 +107,7 @@ describe("costs card", () => {
     });
 
     expect(chartSpy).toHaveBeenCalledWith(
-      "costsPieContainerId",
+      "costsPieContainer",
       expect.objectContaining({
         chart: expect.objectContaining({
           height: 100,
@@ -132,7 +132,7 @@ describe("costs card", () => {
     });
 
     expect(chartSpy).toHaveBeenCalledWith(
-      "costsPieContainerId",
+      "costsPieContainer",
       expect.objectContaining({
         chart: expect.objectContaining({
           height: 100,
