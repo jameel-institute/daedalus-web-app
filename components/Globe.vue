1161f9c14f1adc97db8b7799d7f331dd3a552fbb
David Mears <clarinet.david@gmail.com>
2024-10-21 17:31:55 +0100

    WIP globe component - no disputed areas yet shown

--- components/Globe.vue
+++ components/Globe.vue
@@ -10,15 +10,19 @@
 <script lang="ts" setup>
 import * as am5 from "@amcharts/amcharts5";
 import * as am5map from "@amcharts/amcharts5/map";
+import worldOutline from "@amcharts/amcharts5-geodata/worldOutlineLow";
 // import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow";
 // import am5geodata_worldHigh from "@amcharts/amcharts5-geodata/worldHigh";
 
-import simplified_WHO_adm0 from "@/assets/geodata/simplified_WHO_adm0";
-import WHO_onlyThailand from "@/assets/geodata/WHO_onlyThailand";
+// import simplified_WHO_adm0 from "@/assets/geodata/simplified_WHO_adm0";
+
+import WHONationalBorders from "@/assets/geodata/0_point_02_try_again_who_adm0";
+import WHODisputedAreas from "@/assets/geodata/0_point_02_try_again_who_disputed_areas";
 
 // Refer to old branch: 'globe-for-meeting'
 
 let root: am5.Root;
+const imperialBlue = am5.color("#0000cd");
 
 // TODO: Make it zoom in slightly on load, for a fun animation
 
@@ -31,7 +35,8 @@ const pageMounted = ref(false);
 // For such cases amCharts provides a wrapper function: am5.ready()
 // The above will ensure that the creation of the root element will be delayed until DOM is fully loaded.
 
-let chart;
+let chart: am5map.MapChart;
+
 let zoomControl: am5map.ZoomControl;
 let animation: { pause: () => void, stopped: boolean };
 // Disable zoom controls for now since there is a bug where the first click on the zoom in button
@@ -50,17 +55,23 @@ const applyGlobeSettings = () => {
   }
 };
 
+const removeSeries = (seriesToRemove: am5map.MapPolygonSeries) => {
+  chart.series.removeIndex(
+    chart.series.indexOf(seriesToRemove),
+  ).dispose();
+};
+
 watchEffect(() => {
   if (globediv.value !== null && !root) {
     root = am5.Root.new(globediv.value);
 
     // Set button colors, affecting zoom controls
-    root.interfaceColors.set("primaryButton", am5.color("#0000cd")); // "Imperial Blue"
+    // root.interfaceColors.set("primaryButton", am5.color("#0000cd")); // "Imperial Blue"
 
     chart = root.container.children.push(
       am5map.MapChart.new(root, {
-        panX: "rotateX",
-        panY: "rotateY",
+        panX: "none", // Disable drag-to-rotate for v1 of app (was "rotateX")
+        panY: "none", // Disable drag-to-rotate for v1 of app (was "rotateY")
         projection: am5map.geoOrthographic(),
         paddingBottom: 20,
         paddingTop: 20,
@@ -73,33 +84,76 @@ watchEffect(() => {
       }),
     );
 
-    const polygonSeries = chart.series.push(
-      am5map.MapPolygonSeries.new(root, {
-        geoJSON: simplified_WHO_adm0, // was worldLow for performance
-        reverseGeodata: true,
-        fill: am5.color("#002e4e"), // Dark blue
-      }),
-    );
+    // We only need to draw the boundaries of countries that are available in the model.
+    // So we start by painting a backdrop of the world's geographical outline.
+    const worldOutlineSeries = am5map.MapPolygonSeries.new(root, {
+      geoJSON: worldOutline,
+      fill: imperialBlue,
+    });
+    worldOutlineSeries.mapPolygons.template.setAll({
+      brightness: 0.2,
+    });
+    chart.series.push(worldOutlineSeries);
+    // Next, we paint over that outline, for each of the countries that are available in the model.
+    const countryOptionSeries = am5map.MapPolygonSeries.new(root, {
+      geoJSON: WHONationalBorders,
+      reverseGeodata: true,
+      include: appStore.globeParameter?.options?.map(o => o.id),
+      fill: imperialBlue,
+    });
+    countryOptionSeries.mapPolygons.template.setAll({
+      brightness: 0.4,
+    });
+    chart.series.push(countryOptionSeries);
+
+    const oneCountrySeries = am5map.MapPolygonSeries.new(root, {
+      geoJSON: WHONationalBorders.features.find(f => f.id === "THA"),
+      reverseGeodata: false,
+      fill: imperialBlue,
+    });
+    oneCountrySeries.mapPolygons.template.setAll({
+      brightness: 1,
+      strokeWidth: 2,
+    });
+    chart.series.push(oneCountrySeries);
+
+    // const polygonSeries = chart.series.push(
+    //   am5map.MapPolygonSeries.new(root, {
+    //     geoJSON: WHONationalBorders, // was worldLow for performance
+    //     fill: am5.color("#002e4e"), // Dark blue
+    //   }),
+    // );
 
     // chart.series.push(
     //   am5map.MapPolygonSeries.new(root, {
-    //     geoJSON: simplified_WHO_map_disp_area,
+    //     geoJSON: WHODisputedAreas,
     //     reverseGeodata: true,
     //     fill: am5.color("#161a1d"), // "Imperial Black"
     //   }),
     // );
 
-    chart.series.push(
-      am5map.MapPolygonSeries.new(root, {
-        geoJSON: WHO_onlyThailand,
-        reverseGeodata: true,
-        fill: am5.color("#379f9f"), // "Imperial Seaglass"
-      }),
-    );
+    // chart.series.push(
+    //   am5map.MapPolygonSeries.new(root, {
+    //     geoJSON: WHO_onlyThailand,
+    //     reverseGeodata: true,
+    //     fill: am5.color("#379f9f"), // "Imperial Seaglass"
+    //   }),
+    // );
 
-    polygonSeries.mapPolygons.template.events.on("click", (ev) => {
-      polygonSeries.zoomToDataItem(ev.target.dataItem);
-    });
+    // const USASeries = am5map.MapPolygonSeries.new(root, {
+    //   geoJSON: WHONationalBorders.features.find((d) => d.id === "USA"),
+    //   reverseGeodata: false,
+    //   fill: am5.color("#444444"),
+    // });
+    // chart.series.push(USASeries);
+
+    // setTimeout(() => {
+    //   removeSeries(USASeries);
+    // }, 4000);
+
+    // polygonSeries.mapPolygons.template.events.on("click", (ev) => {
+    //   polygonSeries.zoomToDataItem(ev.target.dataItem);
+    // });
 
     animation = chart.animate({
       key: "rotationX",
