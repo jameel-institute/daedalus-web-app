10bafd71b497aac5154d304307f0d564a7ad1ca2
Emma <EmmaRussellKilburn@gmail.com>
2024-10-22 13:37:30 +0100

    user smaller-icon for help icon in ParameterHeader

--- components/ParameterHeader.vue
+++ components/ParameterHeader.vue
@@ -3,7 +3,7 @@
   <CFormLabel :id="`${parameter.id}-label`" :for="parameter.id">
     {{ parameter.label }}
   </CFormLabel>
-  <TooltipHelp :help-text="parameter.description" :classes="['ms-auto', 'me-3', 'mt-1']" />
+  <TooltipHelp :help-text="parameter.description" :classes="['ms-auto', 'me-3', 'mt-1', 'smaller-icon']" />
 </template>
 
 <script setup lang="ts">
