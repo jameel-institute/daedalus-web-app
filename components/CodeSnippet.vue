3d439e4bd3fbdd3cc4c7850db94ec001e7dd4c47
Emma <EmmaRussellKilburn@gmail.com>
2024-10-22 15:47:47 +0100

    copy snippet to clipboard

--- components/CodeSnippet.vue
+++ components/CodeSnippet.vue
@@ -35,13 +35,18 @@
           for installation instructions and further details.
         </p>
         <div class="code p-3">
-          <pre>daedalus::daedalus(
-  "{{ parameters!.country }}",
-  "{{ parameters!.pathogen }}",
-  response_strategy = "{{ parameters!.response }}",
-  response_threshold = {{ parameters!.hospital_capacity }},
-  vaccine_investment = "{{ parameters!.vaccine }}"
-)</pre>
+          <button
+            id="btn-copy-code"
+            class="btn btn-clipboard btn-sm"
+            :class="copied ? 'btn-copied' : ''"
+            aria-label="Copy code snippet"
+            @click="copySnippet"
+            @blur="resetCopied"
+            @mouseleave="resetCopied"
+          >
+            {{ copied ? "Copied!" : "Copy" }}
+          </button>
+          <pre>{{ codeSnippet }}</pre>
         </div>
       </CModalBody>
     </CModal>
@@ -54,12 +59,47 @@ import { CIcon } from "@coreui/icons-vue";
 const appStore = useAppStore();
 
 const modalVisible = ref(false);
+const copied = ref(false);
 
 const parameters = computed(() => appStore.currentScenario.parameters);
+const codeSnippet = computed(() => {
+  return `daedalus::daedalus(
+  "${parameters.value!.country}",
+  "${parameters.value!.pathogen}",
+  response_strategy = "${parameters.value!.response}",
+  response_threshold = ${parameters.value!.hospital_capacity},
+  vaccine_investment = "${parameters.value!.vaccine}"
+)`;
+});
+
+const copySnippet = () => {
+  navigator.clipboard.writeText(codeSnippet.value);
+  copied.value = true;
+};
+
+const resetCopied = () => {
+  copied.value = false;
+};
 </script>
 
 <style lang="scss" scoped>
   .code {
     background-color: var(--cui-tertiary-bg);
+    position: relative;
+  }
+
+  .btn-clipboard {
+      position: absolute;
+      top: 0.5rem;
+      right: 0.5rem;
+      border-radius: 0.25rem;
+    &:hover {
+      color: var(--cui-light);
+      background-color: var(--cui-primary);
+    }
+  }
+
+  .btn-copied {
+    background-color: var(--cui-secondary-bg-dark)!important;
   }
 </style>
