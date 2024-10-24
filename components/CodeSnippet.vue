ab6591926da9354e52821e5eb027b34bf40ab873
Emma <EmmaRussellKilburn@gmail.com>
2024-10-24 12:13:35 +0100

    component unit tests

--- components/CodeSnippet.vue
+++ components/CodeSnippet.vue
@@ -16,41 +16,41 @@
           </CButton>
         </template>
       </CTooltip>
+      <CModal
+        :visible="modalVisible"
+        aria-labelledby="modalTitle"
+        @close="() => { modalVisible = false }"
+      >
+        <CModalHeader>
+          <CModalTitle id="modalTitle">
+            DAEDALUS code snippet
+          </CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+          <p>
+            Use this R code snippet to run the model directly with the daedalus package for the current parameters.
+            See the <NuxtLink to="https://jameel-institute.github.io/daedalus/" target="_blank">
+              daedalus documentation
+            </NuxtLink>
+            for installation instructions and further details.
+          </p>
+          <div class="code p-3">
+            <button
+              id="btn-copy-code"
+              class="btn btn-clipboard"
+              :class="copied ? 'btn-copied' : ''"
+              aria-label="Copy code snippet"
+              @click="copySnippet"
+              @blur="resetCopied"
+              @mouseleave="resetCopied"
+            >
+              {{ copied ? "Copied!" : "Copy" }}
+            </button>
+            <pre>{{ codeSnippet }}</pre>
+          </div>
+        </CModalBody>
+      </CModal>
     </div>
-    <CModal
-      :visible="modalVisible"
-      aria-labelledby="modalTitle"
-      @close="() => { modalVisible = false }"
-    >
-      <CModalHeader>
-        <CModalTitle id="modalTitle">
-          DAEDALUS code snippet
-        </CModalTitle>
-      </CModalHeader>
-      <CModalBody>
-        <p>
-          Use this R code snippet to run the model directly with the daedalus package for the current parameters.
-          See the <NuxtLink to="https://jameel-institute.github.io/daedalus/" target="_blank">
-            daedalus documentation
-          </NuxtLink>
-          for installation instructions and further details.
-        </p>
-        <div class="code p-3">
-          <button
-            id="btn-copy-code"
-            class="btn btn-clipboard"
-            :class="copied ? 'btn-copied' : ''"
-            aria-label="Copy code snippet"
-            @click="copySnippet"
-            @blur="resetCopied"
-            @mouseleave="resetCopied"
-          >
-            {{ copied ? "Copied!" : "Copy" }}
-          </button>
-          <pre>{{ codeSnippet }}</pre>
-        </div>
-      </CModalBody>
-    </CModal>
   </div>
 </template>
 
@@ -65,11 +65,11 @@ const copied = ref(false);
 const parameters = computed(() => appStore.currentScenario.parameters);
 const codeSnippet = computed(() => {
   return `model_result <- daedalus::daedalus(
-  "${parameters.value!.country}",
-  "${parameters.value!.pathogen}",
-  response_strategy = "${parameters.value!.response}",
-  response_threshold = ${parameters.value!.hospital_capacity},
-  vaccine_investment = "${parameters.value!.vaccine}"
+  "${parameters.value?.country}",
+  "${parameters.value?.pathogen}",
+  response_strategy = "${parameters.value?.response}",
+  response_threshold = ${parameters.value?.hospital_capacity},
+  vaccine_investment = "${parameters.value?.vaccine}"
 )`;
 });
 
--- tests/unit/components/CodeSnippet.spec.ts
+++ tests/unit/components/CodeSnippet.spec.ts
@@ -3,37 +3,109 @@ import { mountSuspended } from "@nuxt/test-utils/runtime";
 import CodeSnippet from "~/components/CodeSnippet.vue";
 import { emptyScenario, mockPinia } from "~/tests/unit/mocks/mockPinia";
 
-describe("code snippet", () => {
-  const stubs = {
-    CIcon: true,
-  };
+const stubs = {
+  CIcon: true,
+};
 
+const parameters = {
+  country: "THA",
+  pathogen: "influenza_1918",
+  response: "none",
+  hospital_capacity: "5500",
+  vaccine: "high",
+};
+
+const expectedSnippet = `model_result <- daedalus::daedalus(
+  "THA",
+  "influenza_1918",
+  response_strategy = "none",
+  response_threshold = 5500,
+  vaccine_investment = "high"
+)`;
+
+const mockCopyToClipboard = vi.fn();
+const originalNavigator = { ...globalThis.navigator };
+
+describe("code snippet", () => {
   const render = async () => {
     return await mountSuspended(CodeSnippet, {
       global: { stubs, plugins: [mockPinia({
-        currentScenario: { ...emptyScenario, parameters: { country: "THA" } },
+        currentScenario: { ...emptyScenario, parameters },
       })] },
     });
   };
 
+  beforeEach(() => {
+    globalThis.navigator = {
+      clipboard: {
+        writeText: mockCopyToClipboard,
+      },
+    } as any;
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.resetAllMocks();
+    globalThis.navigator = originalNavigator;
+    vi.useRealTimers();
+  });
+
   it("renders as expected", async () => {
     const component = await render();
     expect(component.findComponent(CTooltip).props("content")).toBe("Generate code snippet");
-    expect(component.find("button").attributes("aria-label")).toBe("Generate code snippet");
+    expect(component.find("button.btn-scenario-header").attributes("aria-label")).toBe("Generate code snippet");
     expect(component.findComponent(CModal).props("visible")).toBe(false);
   });
 
-  it("shows modal with expected snippet on button click");
+  it("shows modal with expected snippet on button click", async () => {
+    const component = await render();
+    await component.find("button.btn-scenario-header").trigger("click");
+    expect(component.findComponent(CModal).props("visible")).toBe(true);
+    expect(component.find("a").attributes("href")).toBe("https://jameel-institute.github.io/daedalus/");
 
-  it("hides modal on close");
-
-  it("copies snippet on Copy button click", () => {
-    // TODO: and updates button text
+    expect(component.find("pre").text()).toBe(expectedSnippet);
   });
 
-  it("resets Copied view on button blur");
+  it("copies snippet on Copy button click", async () => {
+    const component = await render();
+    await component.find("button.btn-scenario-header").trigger("click");
+    const copyBtn = component.find("#btn-copy-code");
+    expect(copyBtn.text()).toBe("Copy");
+    await copyBtn.trigger("click");
+    expect(mockCopyToClipboard).toHaveBeenCalledWith(expectedSnippet);
+    expect(copyBtn.text()).toBe("Copied!");
+  });
 
-  it("resets Copied view on button mouseleave");
+  const expectCopyTextResetAfterDelay = async (event: string) => {
+    const component = await render();
+    await component.find("button.btn-scenario-header").trigger("click");
+    const copyBtn = component.find("#btn-copy-code");
+    await copyBtn.trigger("click");
+    await copyBtn.trigger(event);
+    // Text should not reset immediately..
+    expect(copyBtn.text()).toBe("Copied!");
+    // ..but after a delay
+    vi.advanceTimersByTime(1500);
+    await nextTick();
+    expect(copyBtn.text()).toBe("Copy");
+  };
 
-  it("renders nothing if no parameters");
+  it("resets Copied text on button blur, after delay", async () => {
+    await expectCopyTextResetAfterDelay("blur");
+  });
+
+  it("resets Copied view on button mouseleave, after delay", async () => {
+    await expectCopyTextResetAfterDelay("mouseleave");
+  });
+
+  it("renders nothing if no parameters", async () => {
+    const component = await mountSuspended(CodeSnippet, {
+      global: { stubs, plugins: [mockPinia({
+        currentScenario: { ...emptyScenario },
+      })] },
+    });
+    expect(component.findComponent(CTooltip).exists()).toBe(false);
+    expect(component.find("button").exists()).toBe(false);
+    expect(component.findComponent(CModal).exists()).toBe(false);
+  });
 });
