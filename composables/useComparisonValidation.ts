758eed8aaa24670de394be6a03311e56d6cf9c87
David Mears <clarinet.david@gmail.com>
2025-03-27 16:30:54 +0000

    Update composable to use computed instead of refs, to reduce chance of race conditions

--- composables/useComparisonValidation.ts
+++ composables/useComparisonValidation.ts
@@ -1,20 +1,18 @@
 import { MAX_COMPARISON_SCENARIOS, MIN_COMPARISON_SCENARIOS } from "~/components/utils/comparisons";
 
 export const useComparisonValidation = (scenariosToCompareAgainstBaseline: MaybeRefOrGetter<Array<string>>) => {
-  const invalid = ref<boolean>(false);
-  const feedback = ref<string>();
-
-  watch(scenariosToCompareAgainstBaseline, () => {
-    const scenarioOptions = toValue(scenariosToCompareAgainstBaseline);
-    invalid.value = false;
-    if (scenarioOptions.length + 1 < MIN_COMPARISON_SCENARIOS) {
-      invalid.value = true;
-      feedback.value = `Please select at least ${MIN_COMPARISON_SCENARIOS - 1} scenario to compare against the baseline.`;
-    } else if (scenarioOptions.length + 1 > MAX_COMPARISON_SCENARIOS) {
-      invalid.value = true;
-      feedback.value = `You can compare up to ${MAX_COMPARISON_SCENARIOS - 1} scenarios against the baseline.`;
+  const tooFewScenarios = computed(() => toValue(scenariosToCompareAgainstBaseline).length + 1 < MIN_COMPARISON_SCENARIOS);
+  const tooManyScenarios = computed(() => toValue(scenariosToCompareAgainstBaseline).length + 1 > MAX_COMPARISON_SCENARIOS);
+  const invalid = computed(() => {
+    return tooFewScenarios.value || tooManyScenarios.value;
+  });
+  const feedback = computed(() => {
+    if (tooFewScenarios.value) {
+      return `Please select at least ${MIN_COMPARISON_SCENARIOS - 1} scenario to compare against the baseline.`;
+    } else if (tooManyScenarios.value) {
+      return `You can compare up to ${MAX_COMPARISON_SCENARIOS - 1} scenarios against the baseline.`;
     }
-  }, { deep: 1, immediate: true });
+  });
 
   return { invalid, feedback };
 };
