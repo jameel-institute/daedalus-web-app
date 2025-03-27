60538f4063c5a82fcda7c6204d78b258eb19863b
David Mears <clarinet.david@gmail.com>
2025-03-27 12:06:31 +0000

    Use computeds in composable instead of refs, to prevent race conditions

--- composables/useScenarioOptions.ts
+++ composables/useScenarioOptions.ts
@@ -1,34 +1,33 @@
 import { type Parameter, type ParameterOption, TypeOfParameter } from "~/types/parameterTypes";
-import { type ParameterSelectOption, paramOptsToSelectOpts } from "~/components/utils/parameters";
+import { paramOptsToSelectOpts } from "~/components/utils/parameters";
 
 export const useScenarioOptions = (parameterAxis: MaybeRefOrGetter<Parameter | undefined>) => {
   const appStore = useAppStore();
 
-  const baselineOption = ref<ParameterOption>();
-  const nonBaselineOptions = ref<ParameterOption[]>([]);
-  const nonBaselineSelectOptions = ref<ParameterSelectOption[]>([]);
-
-  watchEffect(() => {
+  const baselineOption = computed(() => {
     const axis = toValue(parameterAxis);
-
-    if (!appStore.currentScenario.parameters) {
-      baselineOption.value = undefined;
-    } else if (axis?.parameterType === TypeOfParameter.Numeric) {
+    if (!appStore.currentScenario.parameters || !axis) {
+      return undefined;
+    } else if (axis.parameterType === TypeOfParameter.Numeric) {
       const baselineValue = appStore.currentScenario.parameters[axis.id];
       // TODO: (jidea-229) description should say whether the value is a default, min, max; or empty if user-provided.
       // TODO: (jidea-229) For numeric options, do (locale-based) comma-separation of thousands.
       return { id: baselineValue, label: baselineValue, description: "" } as ParameterOption;
     } else {
-      baselineOption.value = axis?.options?.find((o) => {
+      return axis.options?.find((o) => {
         return o.id === appStore.currentScenario.parameters![axis.id];
       });
     }
+  });
 
-    nonBaselineOptions.value = axis?.options?.filter(({ id }) => {
+  const nonBaselineOptions = computed(() => {
+    return toValue(parameterAxis)?.options?.filter(({ id }) => {
       return baselineOption.value && id !== baselineOption.value?.id;
     }) || [];
+  });
 
-    nonBaselineSelectOptions.value = paramOptsToSelectOpts(nonBaselineOptions.value);
+  const nonBaselineSelectOptions = computed(() => {
+    return paramOptsToSelectOpts(nonBaselineOptions.value);
   });
 
   return {
