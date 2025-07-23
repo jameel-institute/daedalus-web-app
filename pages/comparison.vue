44fe6cab9b35c02791e42e96b754ad0b7f7f7c41
David Mears <clarinet.david@gmail.com>
2025-07-23 14:01:46 +0100

    Revert "Undo adding tabs"
    This reverts commit b5f7e663779fe752b5ae9ce19c1c27cdecde09e2.

--- pages/comparison.vue
+++ pages/comparison.vue
@@ -26,15 +26,42 @@
       </ParameterInfoCard>
     </div>
     <CSpinner v-show="showSpinner" class="ms-3 mb-3 mt-3" />
-    <div v-if="appStore.everyScenarioHasCosts" class="col-12">
-      <div class="d-flex align-items-start mx-2">
-        <CostBasisToggler />
-        <div class="ms-auto">
-          <CostsLegend />
-        </div>
+    <CRow v-if="appStore.everyScenarioHasCosts" class="results-cards-container">
+      <div class="col-12">
+        <CTabs active-item-key="costs">
+          <CTabList variant="underline">
+            <CTab item-key="costs" class="d-flex align-items-end px-4">
+              <CIcon icon="cilBarChart" size="lg" class="mb-1 text-inherit" />
+              <p class="fs-6 m-0 ms-3">
+                Losses
+              </p>
+            </CTab>
+            <CTab item-key="timeseries" class="d-flex align-items-end px-4">
+              <CIcon icon="cilChartLine" size="xl" class="mb-1 text-inherit" />
+              <p class="fs-6 m-0 ms-3">
+                Time series
+              </p>
+            </CTab>
+          </CTabList>
+          <CTabContent class="">
+            <CTabPanel class="pt-3" item-key="costs">
+              <div class="d-flex align-items-start mx-2">
+                <CostBasisToggler />
+                <div class="ms-auto">
+                  <CostsLegend />
+                </div>
+              </div>
+              <CompareCostsChart />
+            </CTabPanel>
+            <CTabPanel item-key="timeseries">
+              <p class="time-series-example">
+                A time series
+              </p>
+            </CTabPanel>
+          </CTabContent>
+        </CTabs>
       </div>
-      <CompareCostsChart />
-    </div>
+    </CRow>
     <h4 class="mt-3">
       Table for debugging
     </h4>
@@ -135,7 +162,7 @@
 </template>
 
 <script setup lang="ts">
-import { CIconSvg } from "@coreui/icons-vue";
+import { CIcon, CIconSvg } from "@coreui/icons-vue";
 import type { Scenario } from "~/types/storeTypes";
 
 const showSpinner = ref(true);
@@ -208,5 +235,46 @@ onUnmounted(() => {
 th, tr, td {
   border: 1px solid black;
   padding: 5px;
+}
+
+:deep(.tab-content) {
+  background: $lighter-background;
+  color: var(--cui-dark-text-emphasis);
+  padding: 1rem;
+  border-radius: 5px;
+  border-top-left-radius: 0;
+  border-color: var(--cui-border-color-translucent);
+  border-style: solid;
+  border-width: 1px;
+}
+
+:deep(.nav-underline) {
+  --cui-nav-link-color: var(--cui-secondary-color);
+  --cui-nav-underline-gap: 0.5rem;
+}
+
+:deep(.nav-underline .nav-link) {
+  color: var(--cui-nav-link-color);
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  border-color: transparent;
+  border-style: solid;
+  border-width: 1px;
+  border-bottom-width: 0.125rem;
+
+  &:hover {
+    border-bottom-color: var(--cui-border-color-translucent);
+    border-color: var(--cui-border-color-translucent);
+    background-color: $light-background;
+  }
+}
+
+:deep(.nav-underline .nav-link.active, .nav-underline .show > .nav-link) {
+  font-weight: unset;
+  color: $primary;
+  background-color: $lighter-background;
+
+  border-color: var(--cui-border-color-translucent);
+  border-bottom-color: currentcolor;
 }
 </style>
