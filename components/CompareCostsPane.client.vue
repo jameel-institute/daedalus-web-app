4eb25cf80c4fde8bfd4acafe16b700403c387f2f
David Mears <clarinet.david@gmail.com>
2025-08-15 16:26:05 +0100

    Update CostsTable to allow multiple scenarios
    Since VSL may vary by scenario (when countries differ), a succinct way of presenting
    up to 6 VSL values was required. Trying out doing it with a tooltip denoted by a (i)
    icon instead of using an asterisk.

--- components/CompareCostsPane.client.vue
+++ components/CompareCostsPane.client.vue
@@ -1,13 +1,23 @@
 <template>
-  <div class="d-flex mx-2">
-    <CompareCostsChart class="w-50" />
-    <div class="d-flex align-items-start justify-content-between gap-4">
-      <CompareCostsLegend />
+  <CRow class="mx-2">
+    <div class="d-flex d-xl-none align-items-start justify-content-between">
       <CostBasisToggler />
+      <CompareCostsLegend />
     </div>
-  </div>
+    <CompareCostsChart class="col-12 col-xl-6" />
+    <div class="col-12 col-xl-6 d-flex flex-column">
+      <div class="d-none d-xl-flex align-items-start justify-content-start gap-4">
+        <CompareCostsLegend />
+        <CostBasisToggler />
+      </div>
+      <CostsTable
+        :scenarios="appStore.currentComparison.scenarios"
+        class="w-full mt-3"
+      />
+    </div>
+  </CRow>
 </template>
 
-<style lang="sass" scoped>
-
-</style>
+<script setup lang="ts">
+const appStore = useAppStore();
+</script>
--- components/TooltipHelp.vue
+++ components/TooltipHelp.vue
@@ -1,12 +1,28 @@
 <template>
   <CTooltip
     v-if="helpText"
-    :content="helpText"
     placement="top"
   >
+    <template #content>
+      <template v-if="Array.isArray(helpText)">
+        <div v-for="(text, index) in helpText" :key="index">
+          {{ text }}
+        </div>
+      </template>
+      <div v-else>
+        {{ helpText }}
+      </div>
+    </template>
     <template #toggler="{ togglerId, on }">
-      <CIconSvg class="icon help-icon opacity-50 p-0" :class="classes">
-        <img src="~/assets/icons/circleQuestion.svg" :aria-describedby="togglerId" v-on="on">
+      <CIconSvg
+        class="icon help-icon opacity-50 p-0"
+        :class="classes"
+      >
+        <img
+          :src="`/icons/${infoIcon ? 'info' : 'circleQuestion'}.svg`"
+          :aria-describedby="togglerId"
+          v-on="on"
+        >
       </CIconSvg>
     </template>
   </CTooltip>
@@ -16,7 +32,13 @@
 import { CIconSvg } from "@coreui/icons-vue";
 
 defineProps<{
-  helpText?: string
+  helpText?: string | string[]
   classes: string[]
+  infoIcon?: boolean
 }>();
+
+// The icon svgs are exceptionally stored in /public to facilitate a simple way of having a dynamic img src attribute:
+// https://www.lichter.io/articles/nuxt3-vue3-dynamic-images/#the-public-folder-strategy
+// Bear in mind that this means the image will be cached by the browser, so to update the image, you must also change
+// the file name.
 </script>
