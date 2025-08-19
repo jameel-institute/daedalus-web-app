3809ede3f33dc1081d4768158a3c52d1df9ba4b2
David Mears <clarinet.david@gmail.com>
2025-08-19 17:25:54 +0100

    Remove debugging table

--- pages/comparison.vue
+++ pages/comparison.vue
@@ -56,108 +56,11 @@
         </CTabs>
       </div>
     </CRow>
-    <h4 class="mt-3">
-      Table for debugging
-    </h4>
-    <table v-if="appStore.currentComparison.scenarios" class="mb-3">
-      <thead>
-        <tr>
-          <th>
-            SCENARIO
-          </th>
-          <th
-            v-for="(value, key) in appStore.currentComparison.scenarios[0]?.parameters"
-            :key="key"
-            :class="{ 'bg-info-subtle': axis === key }"
-          >
-            {{ key }}
-            <span v-if="key === axis">
-              (Axis)
-            </span>
-          </th>
-          <th>
-            Run id
-          </th>
-          <th>
-            Status of run job
-          </th>
-          <th>
-            Job finished?
-          </th>
-          <th>
-            Job successful?
-          </th>
-          <th>
-            First data point in result (total cost)
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="(scenario, index) in appStore.currentComparison.scenarios"
-          :key="index"
-          :class="{ 'text-primary': scenarioIsBaseline(scenario) }"
-        >
-          <td>
-            {{ appStore.getScenarioAxisValue(scenario) }}
-            <span v-if="scenarioIsBaseline(scenario)">
-              (Baseline)
-            </span>
-          </td>
-          <td
-            v-for="(value, key) in scenario.parameters"
-            :key="key"
-            :class="{ 'bg-info-subtle': axis === key }"
-          >
-            {{ value }}
-          </td>
-          <td>
-            {{ scenario.runId!.slice(0, 8) }}...
-          </td>
-          <td>
-            <span v-if="scenario.status.data?.done">
-              {{ scenario.status.data.runStatus }}
-            </span>
-            <span v-else>
-              Loading...
-            </span>
-          </td>
-          <td>
-            <span v-if="scenario.status.data?.done === true">
-              Yes
-            </span>
-            <span v-else-if="scenario.status.data?.done === false">
-              No
-            </span>
-            <span v-else>
-              Unknown
-            </span>
-          </td>
-          <td>
-            <span v-if="scenario.status.data?.runSuccess === true">
-              Yes
-            </span>
-            <span v-else-if="scenario.status.data?.runSuccess === false">
-              No
-            </span>
-            <span v-else>
-              Unknown
-            </span>
-          </td>
-          <td>
-            <span v-if="scenario.result.data?.costs[0].value">
-              {{ totalCost(scenario) }}
-            </span>
-          </td>
-        </tr>
-      </tbody>
-    </table>
   </div>
 </template>
 
 <script setup lang="ts">
 import { CIcon, CIconSvg } from "@coreui/icons-vue";
-import type { Scenario } from "~/types/storeTypes";
 
 const showSpinner = ref(true);
 
@@ -167,18 +70,6 @@ const query = useRoute().query;
 appStore.clearScenario(appStore.currentScenario);
 appStore.downloadError = undefined;
 let statusInterval: NodeJS.Timeout;
-
-const axis = computed(() => appStore.currentComparison.axis);
-
-const totalCost = (scenario: Scenario) => {
-  const cost = scenario?.result?.data?.costs[0].value;
-  if (!cost) {
-    return "No data yet";
-  }
-  const { amount, unit } = abbreviateMillionsDollars(cost);
-  return `$${amount} ${unit}`;
-};
-const scenarioIsBaseline = (scenario: Scenario) => appStore.getScenarioAxisValue(scenario) === appStore.currentComparison.baseline;
 
 watch(() => appStore.metadata, async (newMetadata) => {
   if (newMetadata) {
