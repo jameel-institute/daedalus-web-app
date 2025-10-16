520b17beac4634ea2796ec26d4120de6726b9df5
David Mears <clarinet.david@gmail.com>
2025-10-16 16:09:58 +0100

    Separate multi- and single-scenario costs charts utils into separate files

--- components/Charts/CostsChart.client.vue
+++ components/Charts/CostsChart.client.vue
@@ -14,7 +14,8 @@ import "highcharts/esm/modules/export-data";
 import "highcharts/esm/modules/offline-exporting";
 
 import { chartBackgroundColorOnExporting, chartOptions, contextButtonOptions, getColorVariants, menuItemDefinitionOptions } from "~/components/utils/charts";
-import { costsChartPalette, costsChartSingleScenarioTooltip, costsChartStackLabelFormatter, costsChartYAxisTickFormatter, costsChartYAxisTitle } from "./utils/costCharts";
+import { costsChartPalette, costsChartYAxisTickFormatter, costsChartYAxisTitle } from "./utils/costCharts";
+import { costsChartSingleScenarioTooltip, costsChartStackLabelFormatter } from "./utils/singleScenarioCostCharts";
 import { costAsPercentOfGdp } from "../utils/formatters";
 import { CostBasis } from "~/types/unitTypes";
 import { debounce } from "perfect-debounce";
