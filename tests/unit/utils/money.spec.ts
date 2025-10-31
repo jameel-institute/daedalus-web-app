297f04f5f3b298a6d2c66c5b6ba81b9dd3ee6d5d
David Mears <clarinet.david@gmail.com>
2025-10-31 12:31:11 +0000

    Include file missed out of commits

---
+++ tests/unit/utils/money.spec.ts
@@ -0,0 +1,33 @@
+import { abbreviateMillionsDollars } from "~/utils/money";
+
+describe("abbreviateMillionsDollars", () => {
+  it("should convert numbers to trillions, defaulting to precision of 1", () => {
+    expect(abbreviateMillionsDollars(1234567)).toEqual({ amount: "$1.2", unit: "trillion" });
+    expect(abbreviateMillionsDollars(1234567, true)).toEqual({ amount: "$1.2", unit: "T" });
+    expect(abbreviateMillionsDollars(1234567, true, 2)).toEqual({ amount: "$1.23", unit: "T" });
+  });
+
+  it("should convert numbers to billions, defaulting to precision of 1", () => {
+    expect(abbreviateMillionsDollars(1234)).toEqual({ amount: "$1.2", unit: "billion" });
+    expect(abbreviateMillionsDollars(1234, true)).toEqual({ amount: "$1.2", unit: "B" });
+    expect(abbreviateMillionsDollars(1234, true, 2)).toEqual({ amount: "$1.23", unit: "B" });
+  });
+
+  it("should convert numbers to millions, defaulting to precision of 1", () => {
+    expect(abbreviateMillionsDollars(1.2345)).toEqual({ amount: "$1.2", unit: "million" });
+    expect(abbreviateMillionsDollars(1.2345, true)).toEqual({ amount: "$1.2", unit: "M" });
+    expect(abbreviateMillionsDollars(1.2345, true, 2)).toEqual({ amount: "$1.23", unit: "M" });
+  });
+
+  it("should handle small numbers correctly", () => {
+    expect(abbreviateMillionsDollars(0.0012345)).toEqual({ amount: "<$1", unit: "million" });
+    expect(abbreviateMillionsDollars(0.0012345, true)).toEqual({ amount: "<$1", unit: "M" });
+    expect(abbreviateMillionsDollars(0.0012345, true, 2)).toEqual({ amount: "<$1", unit: "M" });
+  });
+
+  it("should handle zero correctly", () => {
+    expect(abbreviateMillionsDollars(0)).toEqual({ amount: "<$1", unit: "million" });
+    expect(abbreviateMillionsDollars(0, true)).toEqual({ amount: "<$1", unit: "M" });
+    expect(abbreviateMillionsDollars(0, true, 2)).toEqual({ amount: "<$1", unit: "M" });
+  });
+});
